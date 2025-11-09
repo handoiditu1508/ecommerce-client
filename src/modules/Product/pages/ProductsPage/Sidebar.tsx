9c000e5ab9255c100a068122c3dc94c4ce578bf3
Danh <handoiditu1508@gmail.com>
2025-11-09 12:29:27 +0700

    make filter sidebar static

--- src/modules/Product/pages/ProductsPage/Sidebar.tsx
+++ src/modules/Product/pages/ProductsPage/Sidebar.tsx
@@ -1,4 +1,4 @@
-import Drawer from "@mui/material/Drawer";
+import Paper from "@mui/material/Paper";
 import { useTheme } from "@mui/material/styles";
 import FilterCriteria from "./FilterCriteria";
 
@@ -6,25 +6,41 @@ function Sidebar() {
   const theme = useTheme();
 
   return (
-    <Drawer
-      variant="permanent"
-      sx={{ width: 216 }}
-      slotProps={{
-        paper: {
-          sx: {
-            width: 216,
-            boxSizing: "border-box",
-            border: "none",
-            mt: `calc(var(--header-client-height) + ${theme.vars.spacing} * 2)`,
-            borderTop: theme.vars.shape.smallBorder,
-            borderRight: theme.vars.shape.smallBorder,
-            height: `calc(100% - var(--footer-height) - var(--header-client-height) - ${theme.vars.spacing} * 2)`,
-            overflowX: "hidden",
-          },
-        },
-      }}>
+    // fixed sidebar
+    // <Drawer
+    //   variant="permanent"
+    //   sx={{ width: 216 }}
+    //   slotProps={{
+    //     paper: {
+    //       sx: {
+    //         width: 216,
+    //         boxSizing: "border-box",
+    //         border: "none",
+    //         mt: `calc(var(--header-client-height) + ${theme.vars.spacing} * 2)`,
+    //         borderTop: theme.vars.shape.smallBorder,
+    //         borderRight: theme.vars.shape.smallBorder,
+    //         height: `calc(100% - var(--footer-height) - var(--header-client-height) - ${theme.vars.spacing} * 2)`,
+    //         overflowX: "hidden",
+    //       },
+    //     },
+    //   }}>
+    //   <FilterCriteria />
+    // </Drawer>
+    <Paper
+      square
+      sx={{
+        boxShadow: "none",
+        width: 216,
+        boxSizing: "border-box",
+        borderTop: theme.vars.shape.smallBorder,
+        borderRight: theme.vars.shape.smallBorder,
+        height: "fit-content",
+        mb: -10, // overlap with footer margin top
+        overflowX: "hidden",
+      }}
+    >
       <FilterCriteria />
-    </Drawer>
+    </Paper>
   );
 }
 
